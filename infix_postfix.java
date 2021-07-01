78aa0cde0f2ea8ccc912d6d9168f389adeed1efe
sahaana iyer <sahaanaiyer@gmail.com>
2021-07-01 21:19:23 +0530

    infix to postfix conversion

--- infix_postfix.java
+++ infix_postfix.java
@@ -22,10 +22,7 @@ class infix_postfix {
     }
 
     public char peek (infix_postfix s) {
-        if (s.top == -1) {
-            System.out.println("Stack Underflow");
-            return '#';
-        }
+        if (s.top == -1) return '#';
         return s.arr[s.top];
     }
 
@@ -42,22 +39,52 @@ class infix_postfix {
         else return false;
     }
 
-    public boolean isOperand(infix_postfix p, char x) {
-        if (x == '+' || x == '-' || x == '*' || x == '/') return true;
+    public boolean isOperator(char x) {
+        if (x == '+' || x == '-' || x == '*' || x == '/' || x == '(' || x == ')') return true;
         else return false;
+    }
+
+    public int priority(char x) {
+        if (x == '(' || x == '[' || x == '{') return 0;
+        else if (x == '*' || x == '/') return 2;
+        else if (x == '+' || x == '-') return 1;
+        else return 0;
     }
 
     public static void main (String[] args) {
         Scanner sc = new Scanner (System.in);
         infix_postfix p = new infix_postfix();
         String s = sc.next();
-        StringBuffer val = new StringBuffer();
-        int x;
-        char op1, op2, op;
-        op1 = Character.toString(s.charAt(0));
-        for (int i=1; i<s.length(); i++) {
-            
+        char[] op = new char[s.length()+1];
+        int k = 0;
+        for (int i=0; i<s.length(); i++) {
+            if (!p.isOperator(s.charAt(i))) 
+                op[k++] = s.charAt(i);
+            else {
+                if (s.charAt(i) == '(') 
+                    p.push(p, s.charAt(i));
+                else if (p.priority(s.charAt(i)) > p.priority(p.peek(p)))
+                    p.push(p, s.charAt(i));
+                else if (s.charAt(i) == ')') {
+                    while (p.peek(p) != '(') {
+                        char popped = p.pop(p);
+                        if (popped != '#') op[k++] = popped;
+                    }
+                    p.pop(p);
+                }
+                else {
+                    while (p.priority(p.peek(p)) >= p.priority(s.charAt(i))) {
+                        char popped = p.pop(p);
+                        if (popped != '#') op[k++] = popped;
+                    }
+                    p.push(p, s.charAt(i));
+                }
+            }
         }
-        
+        while (p.top != -1) {
+            op[k++] = p.pop(p);
+        }
+        op[k] = '\0';
+        System.out.println("Equivalent postfix expression of " + s + " is " + String.valueOf(op));
     }
 }
